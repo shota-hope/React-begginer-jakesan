a4302b1774531f4c0ae31ccbc5534bf6fe1e93de
おおの <74855190+shota-hope@users.noreply.github.com>
2022-06-09 15:32:40 +0900

    Finish 4-1

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import { StrictMode } from "react";
+import React from "react";
 import ReactDom from "react-dom";
 
-import App from "./App";
+const App = () => {
+  return (
+    <>
+      <h1>こんにちは！</h1>
+      <p>お元気ですか？</p>
+    </>
+  );
+};
 
-ReactDom.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  document.getElementById("root")
-);
+ReactDom.render(<App />, document.getElementById("root"));
